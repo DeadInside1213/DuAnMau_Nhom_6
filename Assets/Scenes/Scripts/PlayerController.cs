9fd98fd448ace990d362fcff8f20be75c81995b6
KhanGsGodXs <khang200575@gmail.com>
2024-05-29 18:10:26 +0700

    1.1

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerControl: MonoBehaviour
 {
     [Header("References")]
     private Rigidbody2D rb;
@@ -11,7 +11,13 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private int healthPoints = 100;
     [SerializeField] private float jumpForce = 2f;
-    // Update is called once per frame
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,15 +25,31 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        CheckGround();
     }
+
     void Update()
     {
         Move();
+        Jump();
     }
+
     private void Move()
     {
-        var move = Input.GetAxisRaw("HorizontalInput");
-        
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y).normalized;
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }

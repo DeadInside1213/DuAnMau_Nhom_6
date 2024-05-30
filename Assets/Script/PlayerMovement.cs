7004b201eb64406cac789d797f1dd905f3f07457
hungoio <156114815+hungoio@users.noreply.github.com>
2024-05-30 23:38:21 +0700

    up

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        body.velocity = new Vector3(Input.GetAxis("Horizontal"),body.velocity.y);
+    }
+}

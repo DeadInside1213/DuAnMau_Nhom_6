729dd26dde0b066ac62d73fa1e9ad4eca9f3f2ac
KhanGsGodXs <khang200575@gmail.com>
2024-06-06 12:41:01 +0700

    2

--- Assets/Script/ItemCollector.cs
+++ Assets/Script/ItemCollector.cs
@@ -1,32 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    [Header("Attributes")]
+    [SerializeField] private int score;
 
     string[] itemTag = { "Item", "Coin" };
+
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateScoreText();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update the score text in the UI
+    void UpdateScoreText()
     {
-
+        scoreText.text = "Score: " + score;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // This method is called when the collider enters a trigger collider
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Coin")
         {
-            SendMessage("UpdateScore", 1000);
+            score += 1000;
+            UpdateScoreText();
             Destroy(collision.gameObject);
-
         }
-        if (collision.tag == "Key")
+        else if (collision.tag == "Key")
         {
             Destroy(collision.gameObject);
         }
